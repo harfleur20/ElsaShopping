570a9e9ea9063b0b37bd0dc513e6c65b05c60c54
Francis Kenne <harfleur20@gmail.com>
2026-01-02 14:21:11 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
+import men_banner from "../../FrontEnd/src/assets/Frontend_Assets/banner_mens.png"
+import women_banner from "../../FrontEnd/src/assets/Frontend_Assets/banner_women.png"
+import kid_banner from "../../FrontEnd/src/assets/Frontend_Assets/kid_women.png"
+
+
+
 import Shop from './Pages/Shop'
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
@@ -18,9 +24,9 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/hommes' element={<ShopCategory category="homme"/>}/>
-          <Route path='/femmes' element={<ShopCategory category="femme"/>}/>
-          <Route path='/enfants' element={<ShopCategory category="enfant"/>}/>
+          <Route path='/hommes' element={<ShopCategory banner={men_banner} category="homme"/>}/>
+          <Route path='/femmes' element={<ShopCategory banner={women_banner} category="femme"/>}/>
+          <Route path='/enfants' element={<ShopCategory banner={kid_banner} category="enfant"/>}/>
           
           
           <Route path='/product' element={<Product/>}>
